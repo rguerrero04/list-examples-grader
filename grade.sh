0f6f062d3c04d4fe9ab84c06c918b930e02d5204
Ramon Guerrero <rguerre2004@gmail.com>
2023-05-11 17:21:15 -0700

    Yes

--- grade.sh
+++ grade.sh
@@ -1,3 +1,4 @@
+#set -e
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -8,7 +9,26 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [ -e ./student-submission/ListExamples.java ]
+then 
+    echo "Correct file!"
+    cp -r ./student-submission/ListExamples.java ./grading-area/
+else
+    echo "Wrong file!"
+    exit 1
+fi
 
+if ! javac -cp $CPATH -d ./student-submission/TestListExamples.java
+then
+    echo 'Compilation of tests failed!'
+    exit 1
+fi
+
+if ! javac -cp $CPATH -d ./grading-area/ListExamples.java
+then
+    echo "Compilation of student's code failed!"
+    exit 1
+fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
